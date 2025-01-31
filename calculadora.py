6cb3ad5d557433f5a53ac2204229b54bc157adb8
YeraldoCode <yeramtza@gmail.com>
2025-01-31 01:41:01 -0600

    Funcionamiento de calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,17 +1,62 @@
 import tkinter as tk 
 from tkinter import messagebox
 
-
+#creacion de ventana principal
 ventana = tk.Tk()
-
 ventana.title("CalculadoraYera")
 
 #logo de app
 icono = tk.PhotoImage(file='logo.png')
 ventana.iconphoto(True, icono)
- 
-#color de fondo
-ventana.configure(bg="light blue")
+
+#variable para almacenar el valor de la expresion
+expresion = ""
+
+#variable para almcenar el estado del visor 
+resultado_mostrado = False
+
+
+#funcion para actualizar la expresion en el cuadro de texto 
+def pulsar_tecla(tecla):
+    global expresion, resultado_mostrado
+    #evaluar si se ha calculado y mostrado un resultado
+    if resultado_mostrado:
+    #evaluar si se presiono numero o punto 
+        if tecla.isdigit() or tecla == ".":
+             expresion = str(tecla)
+        else:
+            expresion += str(tecla)
+        resultado_mostrado = False
+    else:
+        expresion += str(tecla)
+
+    visor_texto.set(expresion)
+
+
+#funcion para limpiar
+def limpiar():
+    global expresion
+    global resultado_mostrado
+    expresion = ""
+    visor_texto.set(expresion)    
+    resultado_mostrado = False
+
+#funcion para evaluar la expresion y mostrar el resultado
+def evaluar():
+    global expresion, resultado_mostrado 
+    try:
+        resultado = eval(expresion)
+        #verificar es un numero entero
+        if resultado == int(resultado):
+            resultado = int(resultado)
+        visor_texto.set(str(resultado))
+        expresion = str(resultado)
+        resultado_mostrado = True
+    
+    except:
+        visor_texto.set("Error")
+        expresion = ""
+        resultado_mostrado = False
 
 # Configuración de 5 filas y 4 columnas
 for i in range(5):
@@ -50,19 +95,45 @@ botones = [
 
 # Crear botones en la ventana (excepto el igual)
 for (texto, fila, columna) in botones:
+    if texto == 'C':
+        comando = limpiar
+    else:
+        comando = lambda x=texto: pulsar_tecla(x)
     tk.Button(ventana, 
-              text=texto, padx=20,
-                pady=20).grid(row=fila, 
+                text=texto, 
+                padx=20,
+                pady=20,
+                command=comando
+                ).grid(row=fila, 
                                 column=columna,
                                 sticky='nsew',)
 #boton de igual 
 tk.Button(ventana,
           text="=", padx=20,
           pady=20,
-          font=('Helvetica', 30)).grid(row=5,
+          font=('Helvetica', 30),
+          command=evaluar
+          ).grid(row=5,
+            
                 column=0,
                 columnspan=4,
                 sticky='nsew')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Pantalla
 ventana.mainloop()
